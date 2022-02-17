ba07c63e19d2738b31a38634e5b21c9d74c407ab
akshay dhangar <akshaygdhangar14@gmail.com>
2022-02-18 00:46:04 +0530

    [akshay]commiting changes for arrays program

---
+++ Arraysproblem/2ndlargest.sh
@@ -0,0 +1,30 @@
+for((index=0;index<10;index++))
+do
+	array[index]=$(( (RANDOM%900)+100 ))
+done
+echo "The numbers are: " ${array[@]}
+
+max=${array[0]}
+min=${array[0]}
+for  ind in ${!array[@]}
+do
+
+	if [ $max -lt ${array[ind]} ]
+	then
+	max2=$max
+	max=${array[ind]};
+	elif [[ $max2 -lt ${array[ind]} ]]
+	then
+		max2=${array[ind]}
+	fi
+
+	if [ $min -gt ${array[ind]} ]
+	then
+	min2=$min
+	min=${array[ind]}
+	elif [[ $min2 -gt ${array[ind]} ]]
+	then
+		min2=${array[ind]}
+	fi
+done
+
